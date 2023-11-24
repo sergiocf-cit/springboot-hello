1261d98e991741e862cd18c45b94622749819dd5
Sergio Campos da Fonseca <Sergio.Fonseca-ext@ab-Inbev.com>
2023-11-24 13:30:27 -0300

    upgrade version.

--- src/main/java/sergiocf/springboot/HelloController.java
+++ src/main/java/sergiocf/springboot/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @Value("${build.version}")
     private String buildVersion;
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s! Version: %s", name, buildVersion);
     }
